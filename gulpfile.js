67bfebecfba74fbab75adac2c64bcd17b44e4694
MaximProger <maksimkoshman090@gmail.com>
2022-06-10 12:24:56 +0300

    Добавил webp и изменил сборку

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,189 @@
-import gulp from "gulp"; // Основной модуль
-import { path } from "./gulp/config/path.js"; // Импорт путей
-import { plugins } from "./gulp/config/plugins.js"; // Импорт общих плагинов
+let preprocessor = "sass", // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
+  fileswatch = "html,htm,txt,json,md,woff2"; // List of files extensions for watching & hard reload
 
-// Передаем значения в глобальную переменную
-global.app = {
-  isBuild: process.argv.includes("--build"),
-  isDev: !process.argv.includes("--build"),
-  path: path,
-  gulp: gulp,
-  plugins: plugins,
-};
+import pkg from "gulp";
+const { gulp, src, dest, parallel, series, watch } = pkg;
 
-// Импорт задач
-import { copy } from "./gulp/tasks/copy.js";
-import { reset } from "./gulp/tasks/reset.js";
-import { html } from "./gulp/tasks/html.js";
-import { server } from "./gulp/tasks/server.js";
-import { scss } from "./gulp/tasks/scss.js";
-import { js } from "./gulp/tasks/js.js";
-import { images } from "./gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff, fontStyle } from "./gulp/tasks/fonts.js";
-import { svgSprive } from "./gulp/tasks/svgSprive.js";
-import { zip } from "./gulp/tasks/zip.js";
-import { ftp } from "./gulp/tasks/ftp.js";
+import browserSync from "browser-sync";
+import bssi from "browsersync-ssi";
+import ssi from "ssi";
+import webpackStream from "webpack-stream";
+import webpack from "webpack";
+import TerserPlugin from "terser-webpack-plugin";
+import gulpSass from "gulp-sass";
+import dartSass from "sass";
+import sassglob from "gulp-sass-glob";
+const sass = gulpSass(dartSass);
+import less from "gulp-less";
+import lessglob from "gulp-less-glob";
+import styl from "gulp-stylus";
+import stylglob from "gulp-noop";
+import postCss from "gulp-postcss";
+import cssnano from "cssnano";
+import autoprefixer from "autoprefixer";
+import imagemin from "gulp-imagemin";
+import changed from "gulp-changed";
+import concat from "gulp-concat";
+import rsync from "gulp-rsync";
+import del from "del";
 
-// Наблюдатель за изменениями в файлах
-function watcher() {
-  gulp.watch(path.watch.files, copy);
-  gulp.watch(path.watch.html, html);
-  gulp.watch(path.watch.scss, scss);
-  gulp.watch(path.watch.js, js);
-  gulp.watch(path.watch.images, images);
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+      middleware: bssi({ baseDir: "app/", ext: ".html" }),
+    },
+    ghostMode: { clicks: false },
+    notify: false,
+    online: true,
+    // tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+  });
 }
 
-export { svgSprive };
+function scripts() {
+  return src(["app/js/*.js", "!app/js/*.min.js"])
+    .pipe(
+      webpackStream(
+        {
+          mode: "production",
+          performance: { hints: false },
+          plugins: [
+            new webpack.ProvidePlugin({
+              $: "jquery",
+              jQuery: "jquery",
+              "window.jQuery": "jquery",
+            }), // jQuery (npm i jquery)
+          ],
+          module: {
+            rules: [
+              {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ["@babel/preset-env"],
+                    plugins: ["babel-plugin-root-import"],
+                  },
+                },
+              },
+            ],
+          },
+          optimization: {
+            minimize: true,
+            minimizer: [
+              new TerserPlugin({
+                terserOptions: { format: { comments: false } },
+                extractComments: false,
+              }),
+            ],
+          },
+        },
+        webpack
+      )
+    )
+    .on("error", (err) => {
+      this.emit("end");
+    })
+    .pipe(concat("app.min.js"))
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
+}
 
-// Последовательная обработка шрифтов
-const fonts = gulp.series(ttfToWoff, fontStyle);
+function styles() {
+  return src([`app/scss/*.*`, `!app/scss/_*.*`])
+    .pipe(eval(`${preprocessor}glob`)())
+    .pipe(eval(preprocessor)({ "include css": true }))
+    .pipe(
+      postCss([
+        autoprefixer({ grid: "autoplace" }),
+        cssnano({
+          preset: ["default", { discardComments: { removeAll: true } }],
+        }),
+      ])
+    )
+    .pipe(concat("app.min.css"))
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
+}
 
-export { fonts };
+function images() {
+  return src(["app/images/src/**/*"])
+    .pipe(changed("app/images/dist"))
+    .pipe(imagemin())
+    .pipe(dest("app/images/dist"))
+    .pipe(browserSync.stream());
+}
 
-// Основные задачи
-const mainTasks = gulp.series(gulp.parallel(copy, html, scss, js, images));
-// Построение сценариев выполнения задач
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
-const build = gulp.series(reset, mainTasks);
-const deployZIP = gulp.series(reset, mainTasks, zip);
-const deployFTP = gulp.series(reset, mainTasks, ftp);
+function buildcopy() {
+  return src(
+    [
+      "{app/js,app/css}/*.min.*",
+      "app/images/**/*.*",
+      "!app/images/src/**/*",
+      "app/fonts/**/*",
+    ],
+    { base: "app/" }
+  ).pipe(dest("dist"));
+}
 
-// Экспорт сценариев
-export { dev };
-export { build };
-export { deployZIP };
-export { deployFTP };
+async function buildhtml() {
+  let includes = new ssi("app/", "dist/", "/**/*.html");
+  includes.compile();
+  del("dist/parts", { force: true });
+}
 
-// Выполнение сценария по умолчанию
-gulp.task("default", dev);
+async function cleandist() {
+  del("dist/**/*", { force: true });
+}
+
+function deploy() {
+  return src("dist/").pipe(
+    rsync({
+      root: "dist/",
+      hostname: "username@yousite.com",
+      destination: "yousite/public_html/",
+      // clean: true, // Mirror copy with file deletion
+      include: [
+        /* '*.htaccess' */
+      ], // Included files to deploy,
+      exclude: ["**/Thumbs.db", "**/*.DS_Store"],
+      recursive: true,
+      archive: true,
+      silent: false,
+      compress: true,
+    })
+  );
+}
+
+function startwatch() {
+  watch(`app/scss/**/*`, { usePolling: true }, styles);
+  watch(
+    ["app/js/**/*.js", "!app/js/**/*.min.js"],
+    { usePolling: true },
+    scripts
+  );
+  watch("app/images/src/**/*", { usePolling: true }, images);
+  watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on(
+    "change",
+    browserSync.reload
+  );
+}
+
+export { scripts, styles, images, deploy };
+export let assets = series(scripts, styles, images);
+export let build = series(
+  cleandist,
+  images,
+  scripts,
+  styles,
+  buildcopy,
+  buildhtml
+);
+
+export default series(
+  scripts,
+  styles,
+  images,
+  parallel(browsersync, startwatch)
+);
